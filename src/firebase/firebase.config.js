c5051a648863cff3da57b86b14a18aa084d9a906
muradhossain1 <muradssq12@gmail.com>
2024-12-08 09:54:48 +0600

    Chill gamer peojects Environment Variables setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3GqN1r_1FVX4MJS5PiEFShiqQju-Z384",
-  authDomain: "my-assignment-10-4d054.firebaseapp.com",
-  projectId: "my-assignment-10-4d054",
-  storageBucket: "my-assignment-10-4d054.firebasestorage.app",
-  messagingSenderId: "578520795582",
-  appId: "1:578520795582:web:a933b86bbff524f23d91ef"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
